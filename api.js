d56eb820a051662604c4bf59ead94ef5b4f7eba5
G Mason <gmason0702@gmail.com>
2020-12-10 21:54:36 -0500

    pleaseWork

--- api.js
+++ api.js
@@ -7,7 +7,7 @@ const nextBtn = document.querySelector(".next");
 const previousBtn = document.querySelector(".prev");
 const detailsCard = document.querySelector("card");
 //const cardBody = document.querySelector("card-body");
-const listGroupItem = document.querySelector("list-group-item");
+// const listGroupItem = document.querySelector("list-group-item");
 
 const baseUrl = "https://www.googleapis.com/books/v1/volumes?q=";
 const placeHolder =
@@ -19,17 +19,18 @@ let _totalResults = 0;
 // nextBtn.addEventListener("click", nextPage);
 // previousBtn.addEventListener("click", previousPage);
 searchButton.addEventListener("click", submitSearch);
-listGroupItem.addEventListener("mouseover", getDetails); // how to add an eventListener to an element that doesn't exist yet?
+//listGroupItem.addEventListener("mouseover", getDetails);          // how to add an eventListener to an element that doesn't exist yet?
 
-// searchButton.addEventListener("keypress", function (e) {             //why not??
-//   if (e.key === "Enter") {
-//     submitSearch;
-//   }
-// });
+searchButton.addEventListener("keypress", function (e) {
+  //why not??
+  if (e.key === "Enter") {
+    submitSearch;
+  }
+});
 let pageNumber = 0;
 
 bookListId.style.visibility = "hidden";
-detailsCard.style.visibility = "hidden";
+//detailsCard.style.visibility = "hidden";
 
 function submitSearch(e) {
   fetchResults(e);
@@ -48,7 +49,7 @@ function fetchResults(e) {
     .then(function (json) {
       console.log(json);
       displayResults(json);
-      getDetails(json);
+      //getDetails(json);
       //   createPagination(json);
     });
   // .then(function (details) {
@@ -57,7 +58,7 @@ function fetchResults(e) {
   // });
 }
 
-function displayResults(results) {
+async function displayResults(results) {
   const bookList = results.items;
   //console.log(bookList);
 
@@ -69,6 +70,20 @@ function displayResults(results) {
     para.textContent = "No results found.";
     bookList.appendChild(para);
   }
+  const cardInfo = document.createElement("div");
+  cardInfo.classList.add("card-body");
+
+  const bookImage = document.createElement("img");
+  bookImage.classList.add("card-img-top");
+
+  const cardTitle = document.createElement("h5");
+  cardTitle.classList.add("card-title");
+
+  const cardBlurb = document.createElement("p");
+  cardBlurb.classList.add("card-text");
+
+  const previewBtn = document.createElement("a");
+  previewBtn.classList.add("btn", "btn-primary");
 
   for (let i = 0; i < bookList.length; i++) {
     const book = document.createElement("li");
@@ -77,78 +92,45 @@ function displayResults(results) {
 
     book.classList.add("list-group-item");
     book.innerHTML = current.volumeInfo.title;
-    console.log(book);
-    dispBooks.appendChild(book);
-  }
-  //   const listGroupItem = document.querySelector("list-group-item");
-  //   listGroupItem.addEventListener("mouseover", getDetails); // how to add an eventListener to an element that doesn't exist yet
-  //   getDetails(results);
-}
+    let listGroupItem = document.querySelector(".list-group-item");
+    for (let j = 0; j < 11; j++) {
+      if (current.volumeInfo.imageLinks.smallThumbnail != null) {
+        bookImage.src = `${current.volumeInfo.imageLinks.smallThumbnail}`;
+        bookImage.alt = placeHolder;
+      } else {
+        bookImage.src.innerHTML = placeHolder; //might not need the .innerHTML
+        bookImage.alt.innerHTML = placeHolder;
+      }
 
-function getDetails(results) {
-  //   const listGroupItem = document.querySelector("list-group-item");
-  //   listGroupItem.addEventListener("mouseover", getDetails); // how to add an eventListener to an element that doesn't exist yet?
-
-  const bookList = results.items;
-  console.log(bookList);
-
-  const cardInfo = document.createElement("div");
-  //   cardInfo.className.add("card-body");
-
-  const bookImage = document.createElement("img");
-  //   bookImage.className.add("card-img-top");
-
-  const cardTitle = document.createElement("h5");
-  //   cardTitle.className.add("card-title");
-
-  const cardBlurb = document.createElement("p");
-  //   cardBlurb.className.add("card-text");
-
-  const previewBtn = document.createElement("a");
-  //   previewBtn.classList.add("btn", "btn-primary");
-
-  for (let i = 0; i < bookList.length; i++) {
-    const current = bookList[i];
-    cardInfo.classList.add("card-body");
-    bookImage.classList.add("card-img-top");
-    cardTitle.classList.add("card-title");
-    cardBlurb.classList.add("card-text");
-    previewBtn.classList.add("btn", "btn-primary");
-
-    if (current.volumeInfo.imageLinks.smallThumbnail != null) {
-      bookImage.src.innerHTML = `${current.volumeInfo.imageLinks.smallThumbnail}`;
-      bookImage.alt.innerHTML = placeHolder;
-    } else {
-      bookImage.src.innerHTML = placeHolder; //might not need the .innerHTML
-      bookImage.alt.innerHTML = placeHolder;
+      cardTitle.innerHTML = `${current.volumeInfo.title}`;
+      cardBlurb.innerHTML = `${current.volumeInfo.description}`;
+      previewBtn.innerHTML = `${current.volumeInfo.previewLink}`;
+      console.log(cardTitle);
+      console.log(cardBlurb);
+      console.log(bookImage);
     }
-
-    cardTitle.innerHTML = `${current.volumeInfo.title}`;
-    cardBlurb.innerHTML = `${current.volumeInfo.description}`;
-    previewBtn.innerHTML = `${current.volumeInfo.previewLink}`;
-
-    // if (current.volumeInfo.imageLinks.smallThumbnail != null) {
-    //   bookImage.innerHTML = `<img class="card-img-top" src="${current.volumeInfo.imageLinks.smallThumbnail}" alt="${placeHolder}">`;
-    // } else {
-    //   bookImage.src = placeHolder;
-    //   bookImage.alt = placeHolder;
-    // }
-
-    // cardInfo.innerHTML = `<div class="card-body">`;
-    // bookImage.innerHTML = `<img class="card-img-top" src="${current.volumeInfo.imageLinks.smallThumbnail}" alt="${placeHolder}">`;
-    // cardTitle.innerHTML = `<h5 class="card-title">${current.volumeInfo.title}</h5>`;
-    // cardBlurb.innerHTML = `<p class="card-text">${current.volumeInfo.description}</p>`;
-    // previewBtn.innerHTML = ` <a href="${current.volumeInfo.previewLink}" class="btn btn-primary">Book Preview</a>`;
-    console.log(cardTitle);
-    console.log(cardBlurb);
-    console.log(bookImage);
+    console.log(book);
   }
+  dispBooks.appendChild(book);
+  await listGroupItem.addEventListener("mouseover", function (e) {
+    if (e.target && e.target.matches("li.list-group-item")) {
+      listGroupItem.style.visibility = "visible";
+    } // how to add an eventListener to an element that doesn't exist yet
+  });
   detailsCard.appendChild(cardInfo);
   detailsCard.appendChild(bookImage);
   cardInfo.appendChild(cardTitle);
   cardInfo.appendChild(cardBlurb);
   cardInfo.appendChild(previewBtn);
+
+  // const bookList = results.items;
+  console.log(bookList);
 }
+
+// function getDetails(results) {
+//   //   const listGroupItem = document.querySelector("list-group-item");
+//   //   listGroupItem.addEventListener("mouseover", getDetails); // how to add an eventListener to an element that doesn't exist yet?
+// }
 // function createPagination(json) {
 //   if (_startIndex == 0 && _totalResults > _maxResults) {
 //     let _previousIndex = _startIndex;
